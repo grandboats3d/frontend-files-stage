c41b2691d16d0383f622a9d6abccb799dea3d3b2
CT-PE <yevhenii.prykhodko@vvmd.team>
2026-01-26 13:19:14 +0200

    chore: update API origin in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.appConfig = {
-  apiOrigin: "https://js.backend-stage.grandboats.com/api",
+  apiOrigin: "https://backend-stage.grandboats.com/api",
   frontOrigin: "https://grandboats3d.github.io/frontend-files-stage",
 };
